598d69d24a264f82fbd186ae18360bc5d9bf9880
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-08-07 13:12:19 +0530

    okkk

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,8 +18,7 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res, req) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    // expires: new Date(Date.now() + `${process.env.JWT_COOKIE_EXPIRES_IN}` * 24 * 60 * 60 * 1000),
-    expires: "1d",
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: req.secure
   }
