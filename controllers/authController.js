5d4614af64ecdefd52e281b95eff4b706385d572
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-08-07 12:32:47 +0530

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,7 @@ const Email = require('./../utils/email');
 
 
 const signToken = id => {
-  return jwt.sign({ id }, `${process.env.JWT_SECRET}`, { expiresIn: `${process.env.JWT_EXPIRES_IN}` })
+  return jwt.sign({ id }, `${process.env.JWT_SECRET}`, { expiresIn: "90d" })
 }
 
 const createSendToken = (user, statusCode, res, req) => {
